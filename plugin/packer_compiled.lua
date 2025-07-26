4b3f7ca01c3ffaa9e8346b82996dc78a625b1bb5
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2025-07-26 17:40:20 -0300

    remove lsp file

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,13 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-<<<<<<< HEAD
-local package_path_str = "/home/rick/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?/init.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/rick/.cache/nvim/packer_hererocks/2.1.1702233742/lib/lua/5.1/?.so"
-=======
 local package_path_str = "/home/rick/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1748459687/share/lua/5.1/?/init.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?.lua;/home/rick/.cache/nvim/packer_hererocks/2.1.1748459687/lib/luarocks/rocks-5.1/?/init.lua"
 local install_cpath_pattern = "/home/rick/.cache/nvim/packer_hererocks/2.1.1748459687/lib/lua/5.1/?.so"
->>>>>>> newconfig
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -79,52 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-<<<<<<< HEAD
-  LuaSnip = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/LuaSnip",
-    url = "https://github.com/L3MON4D3/LuaSnip"
-  },
-  ["cmp-nvim-lsp"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-=======
->>>>>>> newconfig
   harpoon = {
     loaded = true,
     path = "/home/rick/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
-<<<<<<< HEAD
-  ["lsp-zero.nvim"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
-    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
-  },
-  ["mason-lspconfig.nvim"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
-    url = "https://github.com/williamboman/mason-lspconfig.nvim"
-  },
-  ["mason.nvim"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/mason.nvim",
-    url = "https://github.com/williamboman/mason.nvim"
-  },
-  ["nvim-cmp"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/nvim-cmp",
-    url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/home/rick/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
-  },
-=======
->>>>>>> newconfig
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/home/rick/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
